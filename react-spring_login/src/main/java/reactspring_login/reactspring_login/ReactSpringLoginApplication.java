5bd71effe274e6f631fa2c7f72e8f231561dc5b9
yikanghee <yikanghee2@naver.com>
2022-04-17 21:13:32 +0900

    d

--- react-spring_login/src/main/java/reactspring_login/reactspring_login/ReactSpringLoginApplication.java
+++ react-spring_login/src/main/java/reactspring_login/reactspring_login/ReactSpringLoginApplication.java
@@ -2,7 +2,17 @@ package reactspring_login.reactspring_login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+
+@EntityScan(basePackageClasses = {
+		Jsr310JpaConverters.class,
+		ReactSpringLoginApplication.class
+})
 @SpringBootApplication
 public class ReactSpringLoginApplication {
 
@@ -10,4 +20,9 @@ public class ReactSpringLoginApplication {
 		SpringApplication.run(ReactSpringLoginApplication.class, args);
 	}
 
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 }
